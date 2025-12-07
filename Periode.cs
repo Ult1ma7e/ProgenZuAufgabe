9d55062169f657d854ab864b61a1bbc5ae078cc5
Ult1ma7e <78754127+Ult1ma7e@users.noreply.github.com>
2025-12-07 14:01:59 +0100

    Add files via upload

---
+++ Periode.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualBasic;
+
+// erstellt von dado1011
+
+namespace EasyBankingBackOffice.Datenhaltung
+
+{
+    public class Periode
+    {
+        // Private fields to store data internaly
+        private DateTime _begin;
+        private DateTime _ende;
+        private int _iD;
+
+
+        /// <summary>
+        /// The get and init methods for each coloumn in the "Periode" table in the database
+        /// </summary>
+        public DateTime Beginn
+        {
+            get { return _begin; }
+            init { _begin = value; }
+        }
+
+        public DateTime Ende
+        {
+            get { return _ende; }
+            init { _ende = value; }
+        }
+
+        public int ID
+        {
+            get { return _iD; }
+            init { _iD = value; }
+        }
+
+        // Empty Constructor
+        public Periode() { }
+
+        // Method to output a custom string representation of the object
+        public override string ToString()
+        {
+            return $"{ID} : {Beginn:dd.MM.yyyy} - {Ende:dd.MM.yyyy}";
+        }
+    }
+}
+
+
---
+++ Processingdaten.cs
@@ -0,0 +1,91 @@
+using System;
+
+// erstellt von dado1011
+
+namespace EasyBankingBackOffice.Datenhaltung
+{
+    public class Processingdaten
+    {
+        // Private fields to store data internaly
+        private int _iD;
+        private double _aktiveKredite;
+        private double _endbestandKapitalmarkt;
+        private double _filialen;
+        private double _mitarbeiter;
+        private double _neueEinlagen;
+        private double _neueKredite;
+        private double _par30Kredite;
+        private double _spareinlagen;
+        private double _transaktionenKapitalmarkt;
+
+
+
+        /// <summary>
+        /// The get and init methods for each coloumn in the "Processingdaten" table in the database
+        /// </summary>
+        public int ID
+        {
+            get { return _iD; }
+            init { _iD = value; }
+        }
+
+        public double AktiveKredite
+        {
+            get { return _aktiveKredite; }
+            init { _aktiveKredite = value; }
+        }
+
+        public double EndbestandKapitalmarkt
+        {
+            get { return _endbestandKapitalmarkt; }
+            init { _endbestandKapitalmarkt = value; }
+        }
+
+        public double Filialen
+        {
+            get { return _filialen; }
+            init { _filialen = value; }
+        }
+
+        public double Mitarbeiter
+        {
+            get { return _mitarbeiter; }
+            init { _mitarbeiter = value; }
+        }
+
+        public double NeueEinlagen
+        {
+            get { return _neueEinlagen; }
+            init { _neueEinlagen = value; }
+        }
+
+        public double NeueKredite
+        {
+            get { return _neueKredite; }
+            init { _neueKredite = value; }
+        }
+
+        public double Par30Kredite
+        {
+            get { return _par30Kredite; }
+            init { _par30Kredite = value; }
+        }
+
+        public double Spareinlagen
+        {
+            get { return _spareinlagen; }
+            init { _spareinlagen = value; }
+        }
+
+        public double TransaktionenKapitalmarkt
+        {
+            get { return _transaktionenKapitalmarkt; }
+            init { _transaktionenKapitalmarkt = value; }
+        }
+
+        // Empty Constructor
+        public Processingdaten() { }
+    }
+}
+
+
